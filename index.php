3f1dbbeea20824172ab431140a1f1b2f10bd1f37
Ahsan Ahmad Raheel <ahsan@cybrain.co.in>
2019-02-16 10:27:55 +0530

    homepage developed

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
+<body>
+    <?php
+        echo "Hello World"
+    ?>
+</body>
 </html>
