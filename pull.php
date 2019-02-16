ee68c398bf661580f7a7e3d57a362b286358a8c9
Ahsan Ahmad Raheel <ahsan@cybrain.co.in>
2019-02-16 11:07:13 +0530

    try

--- pull.php
+++ pull.php
@@ -1,4 +1,12 @@
 <?php
-$output = shell_exec('git pull'); 
-echo "<pre>$output</pre>"; 
+
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+}
+// Print the exec output inside of a pre element
+print("<pre>" . execPrint("git pull") . "</pre>");
 ?>
